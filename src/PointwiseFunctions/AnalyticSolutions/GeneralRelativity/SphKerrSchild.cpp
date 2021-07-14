09f93d89383c72d6a5962ac529df3a7c0210d6bb
marlo <marlo@wheeler.caltech.edu>
2021-07-14 13:33:15 -0700

    Add matrices SphKerr

--- src/PointwiseFunctions/AnalyticSolutions/GeneralRelativity/SphKerrSchild.cpp
+++ src/PointwiseFunctions/AnalyticSolutions/GeneralRelativity/SphKerrSchild.cpp
@@ -20,6 +20,8 @@
 #include "Utilities/StdArrayHelpers.hpp"
 #include "Utilities/StdHelpers.hpp"
 
+#include <iostream>
+
 namespace gr::Solutions {
 
 SphKerrSchild::SphKerrSchild(const double mass,
@@ -49,6 +51,7 @@ void SphKerrSchild::pup(PUP::er& p) noexcept {
   p | center_;
 }
 
+// what is this?
 template <typename DataType, typename Frame>
 SphKerrSchild::IntermediateComputer<DataType, Frame>::IntermediateComputer(
     const SphKerrSchild& solution, const tnsr::I<DataType, 3, Frame>& x,
@@ -61,7 +64,7 @@ void SphKerrSchild::IntermediateComputer<DataType, Frame>::operator()(
     const gsl::not_null<CachedBuffer*> /*cache*/,
     internal_tags::x_sph_minus_center<DataType, Frame> /*meta*/)
     const noexcept {
-  *x_sph_minus_center = x_;
+  *x_sph_minus_center = x_;  // why are you here?
   for (size_t d = 0; d < 3; ++d) {
     x_sph_minus_center->get(d) -= gsl::at(solution_.center(), d);
   }
@@ -116,6 +119,165 @@ void SphKerrSchild::IntermediateComputer<DataType, Frame>::operator()(
   get(*a_dot_x) = spin_a[0] * get<0>(x_sph_minus_center) +
                   spin_a[1] * get<1>(x_sph_minus_center) +
                   spin_a[2] * get<2>(x_sph_minus_center);
+}
+
+// incomplete (TEST)
+template <typename DataType, typename Frame>
+void SphKerrSchild::IntermediateComputer<DataType, Frame>::operator()(
+    const gsl::not_null<tnsr::Ik<DataType, 3, Frame>*> F_matrix,
+    const gsl::not_null<CachedBuffer*> cache,
+    internal_tags::F_matrix<DataType> /*meta*/) const noexcept {
+  const auto spin_a = solution_.dimensionless_spin() * solution_.mass();
+  const auto a_squared =
+      std::inner_product(spin_a.begin(), spin_a.end(), spin_a.begin(), 0.);
+  const auto& rho = get(cache->get_var(internal_tags::rho<DataType>{}));
+  const auto& r_squared =
+      get(cache->get_var(internal_tags::r_squared<DataType>{}));
+
+  auto F_matrix = tnsr::ij<DataVector, Dim, Frame::Inertial> matrix_F{
+      9, -1. / rho / cube(sqrt(r_squared))};
+
+  // std::cout << "the value of matrix_F is" << matrix_F << "\n";
+
+  // F matrix
+  for (int i = 0; i < 3; ++i) {
+    for (int j = i; j < 3; ++j) {
+      if (i == j) {
+        F_matrix.get(i, j) *= (a_squared - spin_a[i] * spin_a[j]);
+      } else {
+        F_matrix.get(i, j) *= -spin_a[i] * spin_a[j];
+      }
+    }
+  }
+}
+
+// incomplete (TEST)
+template <typename DataType, typename Frame>
+void SphKerrSchild::IntermediateComputer<DataType, Frame>::operator()(
+    const gsl::not_null<tnsr::Ij<DataType, 3, Frame>*> P_matrix,
+    const gsl::not_null<CachedBuffer*> cache,
+    internal_tags::P_matrix<DataType> /*meta*/) const noexcept {
+  const auto spin_a = solution_.dimensionless_spin() * solution_.mass();
+  const auto a_squared =
+      std::inner_product(spin_a.begin(), spin_a.end(), spin_a.begin(), 0.);
+  const auto& rho = get(cache->get_var(internal_tags::rho<DataType>{}));
+
+  auto P_matrix = tnsr::Ij<DataVector, Dim, Frame::Inertial> P_matrix{
+      9, -1 / (rho + r) / r};
+
+  // P matrix
+  for (int i = 0; i < 3; ++i) {
+    for (int j = 0; j < 3; ++j) {
+      if (i == j) {
+        P_matrix.get(i, j) *= spin_a[i] * spin_a[j];
+        P_matrix.get(i, j) += rho / r;
+      } else {
+        P_matrix.get(i, j) *= spin_a[i] * spin_a[j];
+      }
+    }
+  }
+}
+
+// incomplete (TEST)
+template <typename DataType, typename Frame>
+void SphKerrSchild::IntermediateComputer<DataType, Frame>::operator()(
+    const gsl::not_null<tnsr::Ij<DataType, 3, Frame>*> jacobian const
+        gsl::not_null<CachedBuffer*>
+            cache,
+    internal_tags::jacobian<DataType> /*meta*/) const noexcept {
+  auto jacobian =
+      tnsr::Ij<DataVector, Dim, Frame::Inertial> jacobian{9, P_matrix};
+
+  // Jacobian
+  for (int i = 0; i < 3; ++i) {
+    for (int j = 0; j < 3; ++j) {
+      for (int k = 0; k < 3; ++k) {
+        jacobian.get(i, j) +=
+            F_matrix.get(i, k) * x_sph_minus_center[k] * x_sph_minus_center[j];
+      }
+    }
+  }
+}
+
+// incomplete (TEST)
+template <typename DataType, typename Frame>
+void SphKerrSchild::IntermediateComputer<DataType, Frame>::operator()(
+    const gsl::not_null<tnsr::Ij<DataType, 3, Frame>*> D_matrix,
+    const gsl::not_null<CachedBuffer*> cache,
+    internal_tags::D_matrix<DataType> /*meta*/) const noexcept {
+  const auto spin_a = solution_.dimensionless_spin() * solution_.mass();
+  const auto a_squared =
+      std::inner_product(spin_a.begin(), spin_a.end(), spin_a.begin(), 0.);
+  const auto& rho = get(cache->get_var(internal_tags::rho<DataType>{}));
+  const auto& r_squared =
+      get(cache->get_var(internal_tags::r_squared<DataType>{}));
+
+  auto D_matrix = tnsr::Ij<DataVector, Dim, Frame::Inertial> D_matrix{
+      9, 1. / cube(rho) / sqrt(r_squared)};
+
+  // D matrix
+  for (int i = 0; i < 3; ++i) {
+    for (int j = i; j < 3; ++j) {
+      if (i == j) {
+        D_matrix.get(i, j) *= (a_squared - spin_a[i] * spin_a[j]);
+      } else {
+        D_matrix.get(i, j) *= -spin_a[i] * spin_a[j];
+      }
+    }
+  }
+}
+
+// incomplete (TEST)
+template <typename DataType, typename Frame>
+void SphKerrSchild::IntermediateComputer<DataType, Frame>::operator()(
+    const gsl::not_null<tnsr::Ij<DataType, 3, Frame>*> C_matrix,
+    const gsl::not_null<CachedBuffer*> cache,
+    internal_tags::C_matrix<DataType> /*meta*/) const noexcept {
+  auto C_matrix =
+      tnsr::Ij<DataVector, Dim, Frame::Inertial> C_matrix{9, D_matrix};
+
+  // D matrix
+  for (int i = 0; i < 3; ++i) {
+    for (int j = i; j < 3; ++j) {
+      C_matrix.get(i, j) += -3. * F_matrix.get(i, j);
+    }
+  }
+}
+
+// incomplete (TEST)
+template <typename DataType, typename Frame>
+void SphKerrSchild::IntermediateComputer<DataType, Frame>::operator()(
+    const gsl::not_null<tnsr::Ij<DataType, 3, Frame>*> deriv_jacobian const
+        gsl::not_null<CachedBuffer*>
+            cache,
+    internal_tags::deriv_jacobian<DataType> /*meta*/) const noexcept {
+  const auto& x_sph_minus_center =
+      cache->get_var(internal_tags::x_sph_minus_center<DataType, Frame>{});
+  const auto& r_squared =
+      get(cache->get_var(internal_tags::r_squared<DataType>{}));
+
+  auto deriv_jacobian =
+      tnsr::Ij<DataVector, Dim, Frame::Inertial> deriv_jacobian{9, F_matrix};
+
+  // deriv_Jacobian
+  for (int k = 0; k < 3; ++k) {
+    for (int i = 0; i < 3; ++i) {
+      for (int j = 0; j < 3; ++j) {
+        deriv_jacobian.get(i, j, k) =
+            F_matrix.get(i, j) * x_sph_minus_center[k] +
+            F_matrix.get(i, k) * x_sph_minus_center[j];
+        for (int m = 0; m < 3; ++m) {
+          if (j == k) {  // j==k acts as a Kronecker delta
+            deriv_jacobian.get(i, j, k) +=
+                F_matrix.get(i, m) * x_sph_minus_center[m];
+          }
+          deriv_jacobian.get(i, j, k) +=
+              C_matrix.get(i, m) * x_sph_minus_center[k] *
+              x_sph_minus_center[m] * x_sph_minus_center[j] / r_squared;
+        }
+      }
+    }
+  }
 }
 
 }  // namespace gr::Solutions
---
+++ tests/Unit/PointwiseFunctions/AnalyticSolutions/GeneralRelativity/Test_SphKerrSchild.cpp
@@ -0,0 +1,48 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <limits>
+#include <string>
+#include <type_traits>
+
+#include "DataStructures/DataBox/Prefixes.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/EagerMath/Magnitude.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Framework/TestCreation.hpp"
+#include "Framework/TestHelpers.hpp"
+#include "Helpers/PointwiseFunctions/AnalyticSolutions/GeneralRelativity/VerifyGrSolution.hpp"
+#include "Helpers/PointwiseFunctions/AnalyticSolutions/TestHelpers.hpp"
+#include "NumericalAlgorithms/LinearOperators/PartialDerivatives.tpp"
+#include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/SphKerrSchild.hpp"
+#include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
+#include "Utilities/ConstantExpressions.hpp"
+#include "Utilities/MakeWithValue.hpp"
+#include "Utilities/TMPL.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+namespace {
+
+
+
+
+
+} // namespace
+
+
+
+
+
+
+SPECTRE_TEST_CASE("Unit.PointwiseFunctions.AnalyticSolutions.Gr.KerrSchild",
+                  "[PointwiseFunctions][Unit]") {
+
+
+
+
+}
