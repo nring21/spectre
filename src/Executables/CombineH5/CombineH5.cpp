b6b527778b5d19fe316ebf47e66d3110e14b8563
Noah <nmr85@cornell.edu>
2022-07-21 12:35:56 -0700

    Fix looping over observationn id's

--- src/Executables/CombineH5/CombineH5.cpp
+++ src/Executables/CombineH5/CombineH5.cpp
@@ -3,7 +3,7 @@
 
 #include <boost/program_options.hpp>
 #include <cstddef>
-// #include <cstdint>  // do we need???
+#include <map>
 #include <string>
 #include <vector>
 
@@ -15,11 +15,6 @@
 #include "NumericalAlgorithms/Spectral/Spectral.hpp"
 #include "Parallel/Printf.hpp"
 #include "Utilities/FileSystem.hpp"
-
-#include <iostream>
-#include <map>
-#include <typeinfo>
-#include <variant>
 
 // Charm looks for this function but since we build without a main function or
 // main module we just have it be empty
@@ -51,29 +46,29 @@ TensorComponent group_tensor_component(
   }
 }
 
-void combine_h5(const std::string& file_prefix,
-                const std::string& subfile_prefix,
-                const std::string& output_prefix) {
+void combine_h5(const std::string& file_prefix, const std::string& subfile_name,
+                const std::string& output) {
+  // Parses for and stores all input files to be looped over
   std::vector<std::string> file_names = file_system::glob(file_prefix + "*.h5");
 
-  // Make new_file
-  h5::H5File<h5::AccessType::ReadWrite> new_file(output_prefix + "0.h5", true);
-  // Add .vol path
-  auto& new_volume_file =
-      new_file.insert<h5::VolumeData>("/" + subfile_prefix + ".vol");
+  // Instantiates the output file and the .vol subfile to be filled with the
+  // combined data later
+  h5::H5File<h5::AccessType::ReadWrite> new_file(output + "0.h5", true);
+  new_file.insert<h5::VolumeData>("/" + subfile_name + ".vol");
   new_file.close_current_object();
 
+  // std::map to store the sorted volume data
   std::map<int, std::vector<
                     std::tuple<size_t, double, std::vector<ElementVolumeData>>>>
       file_map;
 
-  // Open original files
+  // Opens the original files, sorts the volume data, and stores it in the
+  // std::map
   for (size_t i = 0; i < file_names.size(); ++i) {
     h5::H5File<h5::AccessType::ReadWrite> initial_file(
         file_prefix + std::to_string(i) + ".h5", true);
 
-    // Get volume data of file
-    auto& volume_file = initial_file.get<h5::VolumeData>("/" + subfile_prefix);
+    auto& volume_file = initial_file.get<h5::VolumeData>("/" + subfile_name);
 
     auto sorted_volume_data = volume_file.get_data_by_element(
         std::nullopt, std::nullopt, std::nullopt);
@@ -85,17 +80,19 @@ void combine_h5(const std::string& file_prefix,
     initial_file.close_current_object();
   }
 
-  // Append vectors of ElementVolumeData split between H5 files for a given
-  // observation id and value
+  // Append vectors of ElementVolumeData that were split between H5 files for a
+  // given observation id and value
   for (size_t i = 1; i < file_names.size(); ++i) {
-    std::get<2>(file_map.at(0)[0])
-        .insert(std::get<2>(file_map.at(0)[0]).end(),
-                std::get<2>(file_map.at(i)[0]).begin(),
-                std::get<2>(file_map.at(i)[0]).end());
+    for (size_t j = 0; j < file_map.at(0).size(); ++j) {
+      std::get<2>(file_map.at(0)[j])
+          .insert(std::get<2>(file_map.at(0)[j]).end(),
+                  std::get<2>(file_map.at(i)[j]).begin(),
+                  std::get<2>(file_map.at(i)[j]).end());
+    }
   }
 
-  // Write combined volume data for new file
-  auto& new_edited_file = new_file.get<h5::VolumeData>("/" + subfile_prefix);
+  // Write combined volume data for the output file
+  auto& new_edited_file = new_file.get<h5::VolumeData>("/" + subfile_name);
   for (auto const& [observation_id, observation_value, element_data] :
        file_map.at(0)) {
     new_edited_file.write_volume_data(observation_id, observation_value,
@@ -107,17 +104,16 @@ void combine_h5(const std::string& file_prefix,
 
 int main(int argc, char** argv) {
   boost::program_options::positional_options_description pos_desc;
-  //   pos_desc.add("old_spec_cce_file", 1).add("output_prefix", 1);
 
   boost::program_options::options_description desc("Options");
   desc.add_options()("help,h,", "show this help message")(
       "file_prefix", boost::program_options::value<std::string>()->required(),
-      "prefix of files to be combined")(
-      "subfile_prefix",
-      boost::program_options::value<std::string>()->required(),
-      "shared subfile name in each volume file")(
-      "output_prefix", boost::program_options::value<std::string>()->required(),
-      "combined output filename");
+      "prefix of the files to be combined (omit number and file extension)")(
+      "subfile_name", boost::program_options::value<std::string>()->required(),
+      "subfile name shared for each volume file in each H5 file (omit file "
+      "extension)")("output",
+                    boost::program_options::value<std::string>()->required(),
+                    "combined output filename (omit file extension)");
 
   boost::program_options::variables_map vars;
 
@@ -129,12 +125,12 @@ int main(int argc, char** argv) {
       vars);
 
   if (vars.count("help") != 0u or vars.count("file_prefix") == 0u or
-      vars.count("subfile_prefix") == 0u or vars.count("output_prefix") == 0u) {
+      vars.count("subfile_name") == 0u or vars.count("output") == 0u) {
     Parallel::printf("%s\n", desc);
     return 0;
   }
 
   combine_h5(vars["file_prefix"].as<std::string>(),
-             vars["subfile_prefix"].as<std::string>(),
-             vars["output_prefix"].as<std::string>());
+             vars["subfile_name"].as<std::string>(),
+             vars["output"].as<std::string>());
 }
