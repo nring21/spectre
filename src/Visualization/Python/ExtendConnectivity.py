2efa0bc24dda1ec85de8c32caf7abac031958440
DavidWu421 <dwuuu.421@gmail.com>
2022-07-26 10:39:34 -0700

    Time steps for Extend Connectivity python

--- src/Visualization/Python/ExtendConnectivity.py
+++ src/Visualization/Python/ExtendConnectivity.py
@@ -8,76 +8,98 @@ import logging
 import numpy as np
 import sys
 
+import time
+
 
 def generate_grid_point_dictionary(coord_data_x, coord_data_y, coord_data_z):
 
-    gp_dictionary = {}
+    print("Starting generate_grid_point_dictionary")
+    start = time.time()
+
+    grid_point_dictionary = {}
     for i in range(len(coord_data_x)):
-        gp_dictionary.update({
+        grid_point_dictionary.update({
             (coord_data_x[i], coord_data_y[i], coord_data_z[i]):
             i
         })
-    return gp_dictionary
+
+    end = time.time()
+    print("generate_grid_point_dictionary runtime: " + str(end - start))
+    return grid_point_dictionary
 
 
 def sort_and_order(data_list):
 
+    print("Starting sort_and_order")
+    start = time.time()
+
     ordered_coords = []
+    data_list.sort()
     ordered_coords.append(data_list[0])
     for i in range(1, len(data_list)):
-        k = 0
-        for j in ordered_coords:
-            k += 1
-            if (j == data_list[i]):
-                break
-            elif (k == len(ordered_coords)):
-                ordered_coords.append(data_list[i])
-    ordered_coords.sort()
+        if data_list[i] == ordered_coords[-1]:
+            continue
+        else:
+            ordered_coords.append(data_list[i])
+
+    end = time.time()
+    print("sort_and_order runtime: " + str(end - start))
     return ordered_coords
 
 
 def build_connectivity_by_element(sorted_x, sorted_y, sorted_z):
 
+    print("Starting build_connectivity_by_element")
+    start = time.time()
+
     connectivity_as_tuples = []
     for k in range(len(sorted_z) - 1):
         for j in range(len(sorted_y) - 1):
             for i in range(len(sorted_x) - 1):
-                connectivity_as_tuples.append(
-                    (sorted_x[i], sorted_y[j], sorted_z[k]))
-                connectivity_as_tuples.append(
-                    (sorted_x[i + 1], sorted_y[j], sorted_z[k]))
-                connectivity_as_tuples.append(
-                    (sorted_x[i + 1], sorted_y[j + 1], sorted_z[k]))
-                connectivity_as_tuples.append(
-                    (sorted_x[i], sorted_y[j + 1], sorted_z[k]))
-                connectivity_as_tuples.append(
-                    (sorted_x[i], sorted_y[j], sorted_z[k + 1]))
-                connectivity_as_tuples.append(
-                    (sorted_x[i + 1], sorted_y[j], sorted_z[k + 1]))
-                connectivity_as_tuples.append(
-                    (sorted_x[i + 1], sorted_y[j + 1], sorted_z[k + 1]))
-                connectivity_as_tuples.append(
-                    (sorted_x[i], sorted_y[j + 1], sorted_z[k + 1]))
+                connectivity_as_tuples.extend([
+                    (sorted_x[i], sorted_y[j], sorted_z[k]),
+                    (sorted_x[i + 1], sorted_y[j], sorted_z[k]),
+                    (sorted_x[i + 1], sorted_y[j + 1], sorted_z[k]),
+                    (sorted_x[i], sorted_y[j + 1], sorted_z[k]),
+                    (sorted_x[i], sorted_y[j], sorted_z[k + 1]),
+                    (sorted_x[i + 1], sorted_y[j], sorted_z[k + 1]),
+                    (sorted_x[i + 1], sorted_y[j + 1], sorted_z[k + 1]),
+                    (sorted_x[i], sorted_y[j + 1], sorted_z[k + 1])
+                ])
+
+    end = time.time()
+    print("build_connectivity_by_element runtime: " + str(end - start))
     return connectivity_as_tuples
 
 
-def new_connectivity(inertial_coords_x, inertial_coords_y, inertial_coords_z):
+def generate_new_connectivity(inertial_coords_x, inertial_coords_y,
+                              inertial_coords_z):
 
-    gp_dictionary = generate_grid_point_dictionary(inertial_coords_x,
-                                                   inertial_coords_y,
-                                                   inertial_coords_z)
+    print("Starting generate_new_connectivity")
+    start = time.time()
+
+    grid_point_dictionary = generate_grid_point_dictionary(
+        inertial_coords_x, inertial_coords_y, inertial_coords_z)
     ordered_x = sort_and_order(inertial_coords_x)
     ordered_y = sort_and_order(inertial_coords_y)
     ordered_z = sort_and_order(inertial_coords_z)
     connectivity_of_tuples = build_connectivity_by_element(
         ordered_x, ordered_y, ordered_z)
     connectivity = []
+    j = 0
     for i in connectivity_of_tuples:
-        connectivity.append(gp_dictionary[i])
+        # print(i)
+        if i == connectivity_of_tuples[0]:
+            continue
+        else:
+            connectivity.append(grid_point_dictionary[i])
+
+    end = time.time()
+    print("generate_new_connectivity runtime: " + str(end - start))
     return connectivity
 
 
-def fix_connectivity(input_file, subfile_name):
+def extend_connectivity(input_file):
 
     if sys.version_info < (3, 0):
         logging.warning("You are attempting to run this script with "
@@ -85,24 +107,29 @@ def fix_connectivity(input_file, subfile_name):
                         "hang or run very slowly using python 2. Please use "
                         "python 3 instead.")
 
-    path_with_file_name = input_file
-    with h5py.File(path_with_file_name, 'r+') as ds:
-        data = ds[subfile_name + ".vol"]
+    with h5py.File(input_file, 'r+') as dataset:
+        subfile_keys = list(dataset.keys())
+        subfile_name = subfile_keys[0]
+        data = dataset[subfile_name]
         observation_keys = list(data.keys())
         for i in range(len(observation_keys)):
-            data_y = np.array(ds[subfile_name + ".vol/" + observation_keys[i] +
-                                 "/InertialCoordinates_y"])
-            data_x = np.array(ds[subfile_name + ".vol/" + observation_keys[i] +
-                                 "/InertialCoordinates_x"])
-            data_z = np.array(ds[subfile_name + ".vol/" + observation_keys[i] +
-                                 "/InertialCoordinates_z"])
-            new_connect = new_connectivity(data_x, data_y, data_z)
-            del ds[subfile_name + ".vol/" + observation_keys[i] +
-                   "/connectivity"]
-            ds.create_dataset(subfile_name + ".vol/" + observation_keys[i] +
-                              "/connectivity",
-                              data=new_connect)
-        ds.close()
+            print("Starting observation key number " + str(i))  ############
+            data_y = np.array(
+                dataset[subfile_name + "/" + observation_keys[i] +
+                        "/InertialCoordinates_y"])
+            data_x = np.array(
+                dataset[subfile_name + "/" + observation_keys[i] +
+                        "/InertialCoordinates_x"])
+            data_z = np.array(
+                dataset[subfile_name + "/" + observation_keys[i] +
+                        "/InertialCoordinates_z"])
+            new_connect = generate_new_connectivity(data_x, data_y, data_z)
+            del dataset[subfile_name + "/" + observation_keys[i] +
+                        "/connectivity"]
+            dataset.create_dataset(subfile_name + "/" + observation_keys[i] +
+                                   "/connectivity",
+                                   data=new_connect)
+        dataset.close()
 
 
 def parse_args():
@@ -122,15 +149,10 @@ def parse_args():
         "The input file with the .h5 extension where the connectivity will be "
         "updated. It must be a single .h5 file. Use the CombineH5 executable "
         "to combine multiple h5 files first.")
-    parser.add_argument(
-        '--subfile-name',
-        required=True,
-        help="Name of the volume data subfile in the H5 files, excluding the "
-        "'.vol' extension")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     input_args = parse_args()
-    fix_connectivity(**vars(input_args))
+    extend_connectivity(**vars(input_args))
